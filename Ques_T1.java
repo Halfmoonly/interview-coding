4fe92bc17ae2fa31d6a698431a5fef10cb121dae
ly <liuyanoutsee@outlook.com>
2024-01-16 11:07:11 +0800

    解最长子串的长度（滑动窗口+HashMap）

--- Ques_T1.java
+++ Ques_T1.java
@@ -2,9 +2,39 @@ package org.lyflexi.me;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
-/*滑动窗口：阿里巴巴找黄金宝箱(V)*/
+/*滑动窗口：阿里巴巴找黄金宝箱(V)：滑动窗口
+* 难度	易
+题目说明	一贫如洗的樵夫阿里巴巴在去砍柴的路上，无意中发现了强盗集团的藏宝地，藏宝地有编号从0-N的箱子，每个箱子上面贴有一个数字。
+阿里巴巴念出一个咒语数字 k ( k<N )，找出连续 k 个宝箱数字和的最大值，并输出该最大值。
+输入描述	第一行输入一个数字字串，数字之间使用逗号分隔，例如: 2,10,-3,-8,40,5。
+1 ≤ 字串中数字的个数 ≤ 100000。
+-10000 ≤ 每个数字 ≤10000。
+第二行输入咒语数字，例如: 4，咒语数字大小小于宝箱的个数。
+输出描述	连续 k 个宝箱数字和的最大值，例如: 39。
+补充说明	无
+------------------------------------------------------
+示例
+示例1
+输入	2,10,-3,-8,40,5
+* 4
+输出	39
+说明	无
+*
+示例2
+输入	8
+1
+输出	8
+说明	无
+
+*
+*
+*
+*
+* */
+
 public class Ques_T1 {
 
     public static void main(String[] args){
@@ -65,7 +95,7 @@ public class Ques_T1 {
 
 
     /*官方题解：并且要求窗口中元素不重复****************************************************/
-/*    public static long maximumSubarraySum(int[] nums, int k) {
+    public static long maximumSubarraySum(int[] nums, int k) {
         int n = nums.length;
         long rst = 0, sum = 0;
         Map<Integer, Integer> map = new HashMap();
@@ -97,5 +127,5 @@ public class Ques_T1 {
         }
 
         return rst;
-    }*/
+    }
 }
---
+++ hotquestion100/src/main/java/org/lyflexi/solutions/Soultion8_LongestSubstring.java
@@ -0,0 +1,59 @@
+package org.lyflexi.solutions;
+
+import sun.security.provider.HashDrbg;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * @Author: ly
+ * @Date: 2024/1/16 10:36
+ */
+/*本题是滑动窗口：
+
+* 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+输入: s = "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+*
+示例 2:
+输入: s = "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1
+*
+示例 3:
+输入: s = "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+*
+*
+*
+* */
+public class Soultion8_LongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+        int answer = 0;
+        if (s==null) return 0;
+        char[] charArray = s.toCharArray();
+        int n = charArray.length;
+        //key是元素值，value是元素索引（右指针）
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = -1;
+        map.put(charArray[0],0);
+        answer = 1;
+        for (int i = 1; i < n; i++) {
+            char c = charArray[i];
+            if (map.containsKey(c)){
+                left = map.get(c);//更新左指针
+            }
+            map.put(c,i);//右指针正常迭代
+            answer = Math.max(answer,i-left);
+        }
+
+
+        return answer;
+
+    }
+}
