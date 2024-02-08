0f9cf3283a9aba624651fbb318cabb01a9972f3c
ly <liuyanoutsee@outlook.com>
2024-02-08 23:25:11 +0800

    64. 最小路径和: 带有数值的二维动态规划

--- leetcodeDp/src/main/java/org/lyflexi/solutions/Solution05_ThiefAndEarn.java
+++ leetcodeDp/src/main/java/org/lyflexi/solutions/Solution05_ThiefAndEarn.java
@@ -33,7 +33,7 @@ import java.util.Scanner;
 解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
      偷窃到的最高金额 = 2 + 9 + 1 = 12 。
 * */
-public class Solution05_Thief {
+public class Solution05_ThiefAndEarn {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
